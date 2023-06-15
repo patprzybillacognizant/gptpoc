8af23a0d9caac7b23c7249042c0c2a6cd234fc45
patprzybillacognizant <92115397+patprzybillacognizant@users.noreply.github.com>
2023-06-15 01:42:19 -0400

    Update client.py

--- scripts/client.py
+++ scripts/client.py
@@ -5,14 +5,6 @@ from collections import defaultdict
 from typing import Any, Dict, Type
 import requests
 
-
-
-
-
-
-
-
-
 from django.conf import settings
 from django.db import IntegrityError
 from django.db.models import Model
